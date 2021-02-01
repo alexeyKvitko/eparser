6f36d1afae34f01eb9f4eed4a942ae7ff0a1e157
alexeyKvitko <alex09079901>
2021-02-01 08:52:11 +0300

    Add project to git

---
+++ src/main/java/tech/madest/eparser/EparserApplication.java
@@ -0,0 +1,13 @@
+package tech.madest.eparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EparserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EparserApplication.class, args);
+	}
+
+}
